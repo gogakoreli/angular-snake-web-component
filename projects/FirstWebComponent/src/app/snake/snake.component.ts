912d4ab2368e61de3be9b8131f29a2aeee3d9496
Goga Koreli <gogakoreli@gmail.com>
2020-04-05 22:40:07 +0400

    move store service

--- projects/FirstWebComponent/src/app/snake/snake.component.ts
+++ projects/FirstWebComponent/src/app/snake/snake.component.ts
@@ -1,27 +1,9 @@
-import { Component, OnInit, Injectable, ɵmarkDirty as markDirty, ChangeDetectionStrategy, HostBinding } from '@angular/core';
-import { fromEvent, Subject, BehaviorSubject, Observable, interval } from 'rxjs';
-import { takeUntil, map, distinctUntilChanged } from 'rxjs/operators';
-import { defaultGameState, directionReducer, tickReducer, renderConsole } from './snake';
+import { Component, OnInit, ɵmarkDirty as markDirty, ChangeDetectionStrategy, HostBinding } from '@angular/core';
+import { fromEvent, Subject, interval } from 'rxjs';
+import { takeUntil, tap } from 'rxjs/operators';
+import { Store } from './store.service';
+import { directionReducer, tickReducer } from './snake';
 import { GameState, Tile } from './models';
-
-@Injectable({ providedIn: 'root' })
-export class Store {
-  private state = new BehaviorSubject<GameState>(defaultGameState());
-
-  public select(): Observable<GameState>
-  public select<T>(selector?: (state: GameState) => T | GameState): Observable<T | GameState> {
-    selector = selector ?? (state => state);
-    return this.state.asObservable().pipe(
-      map(selector),
-      distinctUntilChanged(),
-    );
-  }
-
-  public reduce(reducer: (state: GameState) => GameState) {
-    const newState = reducer(this.state.value);
-    this.state.next(newState);
-  }
-}
 
 const TICK_INTERVAL = 150;
 
@@ -46,25 +28,23 @@ export class SnakeComponent implements OnInit {
   constructor(private store: Store) { }
 
   ngOnInit() {
-    fromEvent(document, 'keydown').pipe(
-      takeUntil(this.unsubscribe$),
-    ).subscribe((event: KeyboardEvent) => {
-      this.store.reduce(state => directionReducer(state, event));
-    });
+    const direction$ = fromEvent(document, 'keydown').pipe(
+      tap((event: KeyboardEvent) => this.store.reduce(state => directionReducer(state, event))),
+    );
 
-    interval(TICK_INTERVAL).pipe(
-      takeUntil(this.unsubscribe$),
-    ).subscribe(() => {
-      this.store.reduce(tickReducer);
-    });
+    const tick$ = interval(TICK_INTERVAL).pipe(
+      tap(() => this.store.reduce(tickReducer)),
+    );
 
-    this.store.select().pipe(
-      takeUntil(this.unsubscribe$),
-    ).subscribe(state => {
-      // renderConsole(state);
-      this.state = state;
-      markDirty(this);
-    });
+    const state$ = this.store.select().pipe(
+      tap(state => {
+        console.log(state);
+        this.state = state;
+        markDirty(this);
+      }),
+    );
+
+    state$.subscribe();
   }
 
   public handleStartClick() {
---
+++ projects/FirstWebComponent/src/app/snake/store.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { distinctUntilChanged, map } from 'rxjs/operators';
+import { GameState } from './models';
+import { defaultGameState } from './snake';
+
+@Injectable({ providedIn: 'root' })
+export class Store {
+  private state = new BehaviorSubject<GameState>(defaultGameState());
+
+  public select(): Observable<GameState>
+  public select<T>(selector?: (state: GameState) => T | GameState): Observable<T | GameState> {
+    selector = selector ?? (state => state);
+    return this.state.asObservable().pipe(
+      map(selector),
+      distinctUntilChanged(),
+    );
+  }
+
+  public reduce(reducer: (state: GameState) => GameState) {
+    const newState = reducer(this.state.value);
+    this.state.next(newState);
+  }
+}
