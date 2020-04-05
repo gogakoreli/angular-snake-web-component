b75f90bd011246a36e77f83fac5be53eca3db64c
Goga Koreli <gogakoreli@gmail.com>
2020-04-05 20:54:04 +0400

    implement snake

--- projects/FirstWebComponent/src/app/snake/snake.component.ts
+++ projects/FirstWebComponent/src/app/snake/snake.component.ts
@@ -1,15 +1,64 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Injectable, ɵmarkDirty as markDirty, ChangeDetectionStrategy } from '@angular/core';
+import { fromEvent, Subject, BehaviorSubject, Observable, interval } from 'rxjs';
+import { takeUntil, map } from 'rxjs/operators';
+import { defaultGameState, getDirectionReducer, tickReducer, render } from './snake';
+import { GameState } from './models';
+
+@Injectable({ providedIn: 'root' })
+export class Store {
+  private state = new BehaviorSubject<GameState>(defaultGameState());
+
+  public select(): Observable<GameState>
+  public select<T>(selector?: (state: GameState) => T | GameState): Observable<T | GameState> {
+    selector = selector ?? (state => state);
+    return this.state.asObservable().pipe(
+      map(selector),
+    );
+  }
+
+  public reduce(reducer: (state: GameState) => GameState) {
+    const newState = reducer(this.state.value);
+    this.state.next(newState);
+  }
+}
+
+const TICK_INTERVAL = 300;
 
 @Component({
   selector: 'app-snake',
   templateUrl: './snake.component.html',
-  styleUrls: ['./snake.component.scss']
+  styleUrls: ['./snake.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SnakeComponent implements OnInit {
 
-  constructor() { }
+  private state: GameState;
 
-  ngOnInit(): void {
+  private unsubscribe$ = new Subject();
+
+  constructor(private store: Store) { }
+
+  ngOnInit() {
+    fromEvent(document, 'keydown').pipe(
+      takeUntil(this.unsubscribe$),
+    ).subscribe((event: KeyboardEvent) => {
+      const directionReducer = getDirectionReducer(event);
+      this.store.reduce(directionReducer);
+    });
+
+    interval(TICK_INTERVAL).pipe(
+      takeUntil(this.unsubscribe$),
+    ).subscribe(() => {
+      this.store.reduce(tickReducer);
+    });
+
+    this.store.select().pipe(
+      takeUntil(this.unsubscribe$),
+    ).subscribe(state => {
+      render(state);
+      this.state = state;
+      markDirty(this);
+    })
   }
 
 }
---
+++ projects/FirstWebComponent/src/app/snake/snake.ts
@@ -0,0 +1,207 @@
+import { Direction, Food, Snake, SnakePart, Game, GameState } from './models';
+import { InputKey, getInputKey } from './input';
+import { defaultMap, randomFood, updateMap } from './map';
+
+export function defaultGameState(): GameState {
+  return {
+    game: defaultGame(),
+    directions: [Direction.East],
+    shouldRender: true,
+  };
+}
+
+function defaultGame(): Game {
+  return {
+    food: defaultFood(),
+    map: defaultMap(),
+    snake: defaultSnake(),
+  };
+}
+
+function defaultSnake(): Snake {
+  const parts = [{ i: 0, j: 0 }, { i: 0, j: 1 }, { i: 0, j: 2 }];
+  return {
+    direction: Direction.East,
+    head: parts[parts.length - 1],
+    length: parts.length,
+    parts,
+    foodEaten: false,
+  };
+}
+
+function defaultFood(): Food {
+  return { i: 0, j: 10 };
+}
+
+export function updateDirection(snake: Snake, direction: Direction): Snake {
+  if (validNextDirection(snake.direction, direction)) {
+    snake = {
+      ...snake,
+      direction,
+    };
+  }
+  return snake;
+}
+
+export function moveToDirection(snake: Snake, food: Food): Snake {
+  let newParts = snake.parts;
+  const newHead = getNewHead(snake);
+  newParts.push(newHead);
+
+  return {
+    ...snake,
+    head: newHead,
+    parts: newParts,
+    length: newParts.length,
+  };
+}
+
+export function snakeFoodEaten(snake: Snake, food: Food) {
+  const foodEaten = hasFoodEaten(snake, food);
+  let parts = snake.parts;
+  if (!foodEaten) {
+    parts = parts.filter((_, i) => i > 0);
+  }
+
+  return {
+    ...snake,
+    foodEaten,
+    parts,
+    length: parts.length,
+  };
+}
+
+/**
+ * @description shouldn't be used from outside of snake
+ * this is mainly for checking by itself to set property foodEaten
+ * @see snake.foodEaten for using from outside
+ */
+function hasFoodEaten(snake: Snake, food: Food): boolean {
+  return snake.head.i === food.i && snake.head.j === food.j;
+}
+
+function getNewHead(snake: Snake) {
+  const head = snake.head;
+  let newHead: SnakePart;
+  switch (snake.direction) {
+    case Direction.North:
+      newHead = { i: head.i - 1, j: head.j };
+      break;
+    case Direction.East:
+      newHead = { i: head.i, j: head.j + 1 };
+      break;
+    case Direction.South:
+      newHead = { i: head.i + 1, j: head.j };
+      break;
+    case Direction.West:
+      newHead = { i: head.i, j: head.j - 1 };
+      break;
+  }
+  return newHead;
+}
+
+function validNextDirection(curr: Direction, next: Direction): boolean {
+  let result = true;
+  switch (curr) {
+    case Direction.North:
+      result = next !== Direction.South;
+      break;
+    case Direction.East:
+      result = next !== Direction.West;
+      break;
+    case Direction.South:
+      result = next !== Direction.North;
+      break;
+    case Direction.West:
+      result = next !== Direction.East;
+      break;
+    case Direction.None:
+      result = false;
+      break;
+  }
+  return result;
+}
+
+export function tick(game: Game, direction: Direction): Game {
+  game = { ...game, snake: updateDirection(game.snake, direction) };
+  game = { ...game, snake: moveToDirection(game.snake, game.food) };
+  game = { ...game, snake: snakeFoodEaten(game.snake, game.food) };
+  game = { ...game, food: randomFood(game, game.snake.foodEaten) };
+
+  game = { ...game, map: updateMap(game.map, game.snake, game.food) };
+
+  return game;
+}
+
+export function tickReducer(state: GameState): GameState {
+  const [curDirection, nextDirection, ...rest] = state.directions;
+
+  let direction = curDirection;
+  if (nextDirection !== undefined) {
+    direction = nextDirection;
+  }
+  const directions =
+    state.directions.length === 1
+      ? state.directions
+      : [nextDirection, ...rest];
+  return {
+    ...state,
+    game: tick(state.game, direction),
+    directions,
+    shouldRender: true,
+  };
+}
+
+export function getDirectionReducer(event: KeyboardEvent) {
+  const newDirection = getDirection(event);
+  return (state: GameState): GameState => ({
+    ...state,
+    directions: [...state.directions, newDirection],
+    shouldRender: false,
+  });
+}
+
+function inputToDirection(inputKey: InputKey): Direction {
+  let res: Direction = Direction.None;
+  switch (inputKey) {
+    case InputKey.Left:
+      res = Direction.West;
+      break;
+    case InputKey.Right:
+      res = Direction.East;
+      break;
+    case InputKey.Down:
+      res = Direction.South;
+      break;
+    case InputKey.Up:
+      res = Direction.North;
+      break;
+  }
+  return res;
+}
+
+function getDirection(event: KeyboardEvent): Direction {
+  const inputKey = getInputKey(event.keyCode);
+  const newDirection = inputToDirection(inputKey);
+  return newDirection;
+}
+
+export function render(state: GameState) {
+  if (state.shouldRender) {
+    const map = state.game.map;
+    const strGrid = map.grid
+      .map((row) =>
+        row
+          .map((item) =>
+            item.isSnakeHead ? '@' : item.isSnake ? 'x' : item.isFood ? '*' : '.',
+          )
+          .join(' '),
+      )
+      .join('\n');
+    console.log(strGrid);
+    console.log();
+  }
+}
+
+
+
