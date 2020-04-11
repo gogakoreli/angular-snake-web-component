f3cf8b53546e93886d68cfcf86f956f6a4de1afb
Goga Koreli <gogakoreli@gmail.com>
2020-04-11 22:33:08 +0400

    remove not needed component

--- projects/FirstWebComponent/src/app/app.module.ts
+++ projects/FirstWebComponent/src/app/app.module.ts
@@ -1,13 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, DoBootstrap, Injector } from '@angular/core';
 import { createCustomElement } from '@angular/elements';
-
-import { UIButtonComponent } from './uibutton/uibutton.component';
 import { SnakeComponent } from './snake/snake.component';
 
 @NgModule({
   declarations: [
-    UIButtonComponent,
     SnakeComponent,
   ],
   imports: [
@@ -19,12 +16,8 @@ import { SnakeComponent } from './snake/snake.component';
 export class AppModule implements DoBootstrap {
 
   constructor(injector: Injector) {
-    const webComponent = createCustomElement(UIButtonComponent, { injector });
-    customElements.define('ui-button', webComponent);
-
     const snakeComponent = createCustomElement(SnakeComponent, { injector });
     customElements.define('ng-snake', snakeComponent);
-
   }
 
   ngDoBootstrap() { }
